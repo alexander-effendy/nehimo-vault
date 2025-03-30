1cf092a8f22ca61447ef517832b2b6183e10c8a5
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2025-03-31 02:02:04 +1100

    UI: small changes in search bar ui

--- frontend/src/component/password/PasswordComponent.tsx
+++ frontend/src/component/password/PasswordComponent.tsx
@@ -2,6 +2,7 @@ import { PasswordComponentProp } from "@/features/category/PasswordSlice";
 import { useState } from "react";
 import { formatCreatedDate } from "../../utils/date";
 import { PiDotsThreeOutlineLight } from "react-icons/pi";
+import { MdContentCopy } from "react-icons/md";
 
 const PasswordComponent: React.FC<PasswordComponentProp> = ({ id, categoryid, usage, username, password, date_created, last_edited}) => {
   const [showPassword, setShowPassword] = useState<boolean>(false);
@@ -12,7 +13,8 @@ const PasswordComponent: React.FC<PasswordComponentProp> = ({ id, categoryid, us
       <span className="w-[25px]">{id}</span>  
       <span className={`w-[150px] truncate`}>{usage}</span>
       <span className="w-[160px] truncate">{username}</span>
-      <span onClick={() => setShowPassword(prevState => !prevState)} className="hover:cursor-pointer w-[160px] truncate">{showPassword ? password : '*****************'}</span>
+      <MdContentCopy className="absolute left-[352px] top-[15px]"/>
+      <span onClick={() => setShowPassword(prevState => !prevState)} className={`flex items-center select-none highlightable hover:cursor-pointer w-[160px] truncate ${!showPassword && 'pt-[5px]'}`}>{showPassword ? password : '*****************'}</span>
       <span className="w-[70px] flex items-center justify-center">{formatCreatedDate(date_created)}</span>
       <PiDotsThreeOutlineLight className="absolute right-7 text-gray-700 hover:text-gray-500 hover:cursor-pointer" size={15}/>
     </div>
